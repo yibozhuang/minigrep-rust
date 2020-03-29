72dcd5e5262b71812e0fee1df60f19df6b7d7408
Yibo Zhuang <yibzhuang@gmail.com>
2020-03-29 11:10:10 -0700

    adding README for release

---
+++ README.md
@@ -0,0 +1,37 @@
+# minigrep-rust
+Rust minigrep exercise
+
+### Install Rust
+Use rustup
+[https://rustup.rs/]
+
+### Setting up Rust in VSCode
+Install RLS extension in VSCode
+[https://github.com/rust-lang/rls-vscode]
+
+### Building and Running
+
+#### Build
+Dev:
+```
+$ cargo build
+```
+
+Release:
+```
+$ cargo build --release
+```
+
+#### Test
+
+```
+$ cargo test
+```
+
+#### Run
+
+```
+$ cargo run
+
+$ cargo run to poem.txt
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use minigrep_yibozhuang::Config;
+use minigrep_yibozhuang::run;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
@@ -9,7 +10,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Appleication error: {}", e);
         process::exit(1);
     }
